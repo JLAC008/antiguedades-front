cb5f64bd7c6c7145ffdb368f4739ea35ec174e35
joseluji223@gmail.com <JLAC>
2026-06-04 21:49:41 +0200

    refactor: Obliga a hacer login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,6 @@ import { authGuard } from './core/auth.guard';
 
 export const routes: Routes = [
   {
-    path: '',
-    loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
-  },
-  {
-    path: 'coleccion',
-    loadComponent: () => import('./pages/collection/collection.component').then(m => m.CollectionComponent)
-  },
-  {
     path: 'login',
     loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
   },
@@ -19,27 +11,39 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent)
   },
   {
-    path: 'catalogo/:id',
-    loadComponent: () => import('./pages/catalog-detail/catalog-detail.component').then(m => m.CatalogDetailComponent)
-  },
-  {
-    path: 'pieza/:id',
-    loadComponent: () => import('./pages/antique-detail/antique-detail.component').then(m => m.AntiqueDetailComponent)
-  },
-  {
-    path: 'subir',
+    path: '',
     canActivate: [authGuard],
-    loadComponent: () => import('./pages/upload-antique/upload-antique.component').then(m => m.UploadAntiqueComponent)
-  },
-  {
-    path: 'editar/:id',
-    canActivate: [authGuard],
-    loadComponent: () => import('./pages/upload-antique/upload-antique.component').then(m => m.UploadAntiqueComponent)
-  },
-  {
-    path: 'catalogos',
-    canActivate: [authGuard],
-    loadComponent: () => import('./pages/manage-catalogs/manage-catalogs.component').then(m => m.ManageCatalogsComponent)
+    loadComponent: () => import('./layouts/main-layout/main-layout.component').then(m => m.MainLayoutComponent),
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+      },
+      {
+        path: 'coleccion',
+        loadComponent: () => import('./pages/collection/collection.component').then(m => m.CollectionComponent)
+      },
+      {
+        path: 'catalogo/:id',
+        loadComponent: () => import('./pages/catalog-detail/catalog-detail.component').then(m => m.CatalogDetailComponent)
+      },
+      {
+        path: 'pieza/:id',
+        loadComponent: () => import('./pages/antique-detail/antique-detail.component').then(m => m.AntiqueDetailComponent)
+      },
+      {
+        path: 'subir',
+        loadComponent: () => import('./pages/upload-antique/upload-antique.component').then(m => m.UploadAntiqueComponent)
+      },
+      {
+        path: 'editar/:id',
+        loadComponent: () => import('./pages/upload-antique/upload-antique.component').then(m => m.UploadAntiqueComponent)
+      },
+      {
+        path: 'catalogos',
+        loadComponent: () => import('./pages/manage-catalogs/manage-catalogs.component').then(m => m.ManageCatalogsComponent)
+      },
+    ]
   },
   { path: '**', redirectTo: '' }
 ];
